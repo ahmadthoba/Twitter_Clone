5c5fb3ec99796e8c9b8f65b03a4af0757502e044
ahmadthoba <thobabackside@gmail.com>
2023-06-07 12:21:11 +0700

    first commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,7 @@
+class AppwriteConstants {
+  static const String databaseId = '63e27402cddef0f2233a';
+  static const String projectId = '63e1c2ac33d8cc06583f';
+  static const String endPoint = '647fed078f3139ba7f04';
+
+  static var usersCollection;
+}
