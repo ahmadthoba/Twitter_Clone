5d66ca581bedbc176b91b4ccdee8c2187d0675ec
Suto <108921554+ahmadthoba@users.noreply.github.com>
2023-07-10 12:27:40 +0700

    appwrite_constants.dart

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,7 +1,6 @@
 class AppwriteConstants {
-  static const String databaseId = '63e27402cddef0f2233a';
-  static const String projectId = '63e1c2ac33d8cc06583f';
-  static const String endPoint = '647fed078f3139ba7f04';
-
-  static var usersCollection;
+  static const String databaseId = '642cf1eecfd1732d3547';
+  static const String projectId = '6423b31cb706821d5e0e';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+  static const String usersCollection = '647fed078f3139ba7f04';
 }
