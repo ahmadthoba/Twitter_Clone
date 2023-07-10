9f3c5c3e7fb15a4bec2a94f5515fe83dde897ec4
Suto <108921554+ahmadthoba@users.noreply.github.com>
2023-07-10 12:05:26 +0700

    home_view.dart

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
-class HomeView extends ConsumerWidget {
-  static route() => MaterialPageRoute(builder: (context) => const HomeView());
-  const HomeView({super.key});
+class CreateTweetScreen extends ConsumerStatefulWidget {
+  static route() => MaterialPageRoute(
+    builder: (context) => const CreateTweetScreen(),
+  );
+  const CreateTweetScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold();
+  ConsumerState<ConsumerStatefulWidget> createState() => _CreateTweetScreenState();
+}
+
+class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    final currentUser = ref.watch(currentUserDetailsProvider).value; 
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.close, size: 30),
+        ),
+        actions: [
+          RoundedSmallButton(
+            onTap: () {}, 
+            label: 'Tweet',
+            backgroundColor: Pallete.blueColor,
+            textColor: Pallete.whiteColor,  
+          ),
+        ],
+      ),
+      body: currentUser == null
+      ? const Loader()
+      : SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(currentUser.profilePic),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
