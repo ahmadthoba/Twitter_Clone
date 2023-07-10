88c137fa69501bb25513b210e81bbf7af82181c5
Suto <108921554+ahmadthoba@users.noreply.github.com>
2023-07-10 12:19:53 +0700

    auth_field.dart

--- lib/features/auth/widgets/auth_field.dart
+++ lib/features/auth/widgets/auth_field.dart
@@ -1,13 +1,12 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import '../../../theme/theme.dart';
 
 class AuthField extends StatelessWidget {
   const AuthField({
-    Key? key,
+    super.key,
     required this.controller,
     required this.hintText,
-  }) : super(key: key);
+  }) ;
   final TextEditingController controller;
   final String hintText;
 
