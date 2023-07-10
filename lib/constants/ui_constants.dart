8af84e8f0086f202c76769cf9e0a9538803375f8
Suto <108921554+ahmadthoba@users.noreply.github.com>
2023-07-10 12:22:44 +0700

    ui_constants.dart

--- lib/constants/ui_constants.dart
+++ lib/constants/ui_constants.dart
@@ -13,4 +13,10 @@ class UIConstants {
       centerTitle: true,
     );
   }
+
+  static List<Widget> bottomTabBarPages = [
+    Text('Feed Screen'),
+    Text('Search Screen'),
+    Text('Notification Screen'),
+  ];
 }
